5d94209a77360b2f486a11aa1c55f7f7e273cdef
Faruk Cankaya <johndoesecretservice007@gmail.com>
2022-06-26 11:32:41 +0200

    lorin fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $( "#search-cat" ).submit(function( event ) {
         $("#not-found").hide();
         var code = $("#search").val();
-        if(code == "90023300057121"){
+        if(code == "900233000571821"){
             window.location.href = "/lorin";
         }else if(code == "900233000571820"){
             window.location.href = "/concon";
